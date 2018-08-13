a15112cfae30a1d53d007d61baab16c1f8c1f1b3
Deon <deonsummers91@gmail.com>
2018-08-13 16:38:15 -0500

    add synchronous funding

--- src/deploy/fundWallets.js
+++ src/deploy/fundWallets.js
@@ -5,30 +5,44 @@ const fs = require('fs');
 const FILENAME = path.resolve(__dirname, process.env.OUTPUT_FILENAME);
 const delay = 3 * 1000 // 2 seconds
 
-console.log(`Sending One transaction every ${delay / 1000} seconds`);
-
-
 module.exports = (callback) => {
   let accounts = JSON.parse(fs.readFileSync(FILENAME, {encoding: "utf8"}));
-  web3.eth.getCoinbase((err, coinbase) => {
+  web3.eth.getCoinbase(async (err, coinbase) => {
     for(let i=0; i<accounts.length; i++) {
-      wait(i).then(() => {
-        web3.eth.sendTransaction({from: coinbase, to: accounts[i].public, value: process.env.INITIAL_WEI}, (err, res) => {
-          if (err) console.error(err);
-          console.log(`sent ${i+1}/${accounts.length} transactions with hash: ${res}`);
-        })
-      })
-      .catch((e) => {
-        console.log('THERE WAS AN ERROR, LOG WRITTEN TO error.log');
-        fs.appendFileSync(path.resolve(__dirname, 'error.log'), `Account ${accounts[i].public} failed: ${e} \n`);
-      });
-    }
+      await transferFunds(coinbase, accounts[i].public)
+    };
   });
 }
 
 
-function wait(i) {
-  return new Promise((res, rej) => {
-    return setTimeout(() => res(true), delay*i);
+function transferFunds(from, to, amount) {
+  return new Promise((resolve, reject) => {
+    web3.eth.sendTransaction({from, to, value: process.env.INITIAL_WEI}, (err, res) => {
+      if (err) console.error(err);
+      console.log(`sent ${to} funds with hash: ${res}`);
+      confirmTransaction(to)
+        .then(() => {
+          resolve(res)
+        });
+    })
+  });
+}
+
+function confirmTransaction(address) {
+  return new Promise((resolve, rej) => {
+    let filter = web3.eth.filter('latest');
+    filter.watch((err, res) => {
+      if(!err) {
+        web3.eth.getBalance(address, (err, res) => {
+          console.log('wasnt included in this block');
+          if(+res > 0) {
+            filter.stopWatching((err, res) => {
+              console.log('included!');
+              resolve();
+            });
+          }
+        });
+      }
+    })
   });
 }
